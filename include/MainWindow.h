7f94a0f02fefd217c2e83da1b4b1b4af89f30916
Neeraj Sharma <neeraj.sharma.70@outlook.com>
2024-11-04 16:09:23 +0530

    add menu

--- include/MainWindow.h
+++ include/MainWindow.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <QMainWindow>
+#include <bar/MenuBar.h>
+#include <menu/FileMenu.h>
 
 class MainWindow : public QMainWindow {
   Q_OBJECT
 public:
   MainWindow(QWidget * parent = nullptr);
   virtual ~MainWindow();
+
+private:
+  void decorateWindow();
+
+private:
+  MenuBar* m_menuBar;
+  FileMenu* m_fileMenu;
 };
--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,8 +1,30 @@
 #include <MainWindow.h>
+#include <QGraphicsDropShadowEffect>
 
 MainWindow::MainWindow(QWidget *parent): QMainWindow(parent) {
+  m_menuBar = new MenuBar(this);
 
+  std::string name("File");
+  m_fileMenu = new FileMenu(name, this);
+  
+  m_menuBar->addMenu(m_fileMenu);
+  this->setMenuBar(m_menuBar);
+
+  decorateWindow();
+  connect(m_fileMenu, &FileMenu::close, this, &MainWindow::close);
+}
+
+void MainWindow::decorateWindow(){
+  QGraphicsDropShadowEffect *shadow = new QGraphicsDropShadowEffect(this);
+  shadow->setBlurRadius(20);
+  shadow->setOffset(0, 5);
+  shadow->setColor(Qt::gray);
+  this->setGraphicsEffect(shadow);
+  setStyleSheet("background-color: white; border-radius: 10px;");
+  resize(400, 300);
 }
 
 MainWindow::~MainWindow() {
+  delete m_fileMenu;
+  delete m_menuBar;
 }
